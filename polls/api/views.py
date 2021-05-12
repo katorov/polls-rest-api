040e8a03139dc5ae4ed68fcaf6bf89736fbe649a
user <user@info.bk>
2021-05-12 23:26:03 +0300

    Set admin permissions to views

--- polls/api/views.py
+++ polls/api/views.py
@@ -1,17 +1,27 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
 from polls import models
 from polls.api import serializers
 from polls.api.mixins import MultiSerializerViewSetMixin
+from polls.api.permissions import ReadOnly
 
 
 class PollViewSet(MultiSerializerViewSetMixin, ModelViewSet):
     """Представление для работы с опросами"""
-    queryset = models.Poll.objects.available()
+    queryset = models.Poll.objects.all()
     serializer_class = serializers.PollSerializer
     serializer_action_classes = {
         'create': serializers.PollCreateSerializer
     }
+    permission_classes = [IsAdminUser | ReadOnly]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        is_admin = bool(self.request.user and self.request.user.is_staff)
+        if not is_admin:
+            return queryset.available()
+        return queryset
 
 
 class QuestionViewSet(MultiSerializerViewSetMixin, ModelViewSet):
@@ -21,3 +31,4 @@ class QuestionViewSet(MultiSerializerViewSetMixin, ModelViewSet):
     serializer_action_classes = {
         'create': serializers.QuestionCreateSerializer
     }
+    permission_classes = [IsAdminUser]
